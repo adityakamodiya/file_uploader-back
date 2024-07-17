6c9b23c4bb69a52d482ffb6bec9073c4d3659a6d
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-07-17 22:59:23 +0530

    ...

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ cloudinary.config({
 });
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }))
+app.use(cors({ origin: "*" }))
 app.use(fileUpload({ useTempFiles: true }))
 app.use(express.urlencoded({ extended: false }))
 
@@ -24,22 +24,23 @@ app.post("/upload", (req, res) => {
 
     let {Name,Number,Email,State,City}   = {...req.body};
 
-    console.log(Name,Number,Email,State,City,file);
+    // console.log(Name,Number,Email,State,City,file);
 
 // res.send("success");
-    // cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
-    //             if (err) {
-    //                 console.error("this is error:" + err);
-    //                 res.status(500).send(JSON.stringify('Error uploading to Cloudinary'));
-    //             } else {
-    //                 console.log(result);
-    //                 console.log(result.secure_url)
-    //                 let url = result.secure_url
-    //                // let detail = db.collection('files').insertOne({Name,Number,Email,State,City,url,publicId})
-    //                 res.status(200).send(JSON.stringify('File uploaded to Cloudinary'));
+    cloudinary.uploader.upload(file.tempFilePath, (err, result) => {
+                if (err) {
+                    console.error("this is error:" + err);
+                    res.status(500).send(JSON.stringify('Error uploading to Cloudinary'));
+                } else {
+                    console.log(result);
+                    console.log(result.secure_url)
+                    let url = result.secure_url
+                     let publicId = result.public_id;
+                   let detail = db.collection('files').insertOne({Name,Number,Email,State,City,url,publicId})
+                    res.status(200).send(JSON.stringify('File uploaded to Cloudinary'));
               
-    //             }
-    //         });
+                }
+            });
 
 
 
@@ -47,7 +48,10 @@ app.post("/upload", (req, res) => {
 });
 
 
-
+app.get("/getdata",async(req,res)=>{
+    let data = await db.collection('files').find().toArray();
+    res.send(data);
+})
 
 // app.post("/try",(req,res)=>{
 //     console.log(req.files.file,req.body.name);
