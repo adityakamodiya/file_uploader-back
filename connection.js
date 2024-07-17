97ea24c0e0fb1c1c9450912588b96c138a00d76e
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-07-18 02:46:28 +0530

    commit

--- connection.js
+++ connection.js
@@ -1,4 +1,4 @@
 import{MongoClient} from "mongodb"
-const connection = MongoClient.connect("mongodb://127.0.0.1:27017")
+const connection = MongoClient.connect("mongodb+srv://adityakamodiya:11223344@cluster0.j4ukslx.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster0")
 export const dbName = "fileuploader"
 export default connection
