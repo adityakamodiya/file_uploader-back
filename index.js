db5d6a6c39384dac7d70f391dab3ac5196119cd0
Aditya Kamodiya <adityakamodiya@gmail.com>
2024-07-18 01:29:55 +0530

    commit

--- index.js
+++ index.js
@@ -15,18 +15,8 @@ cloudinary.config({
 });
 
 app.use(express.json());
-const allowedOrigins = ['https://admin-pannel-pi.vercel.app'];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors({ origin: "*" }))
 app.use(fileUpload({ useTempFiles: true }))
 app.use(express.urlencoded({ extended: false }))
 
